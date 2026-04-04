ccd97a12ddf5b5b56bcd8cf6947cc7b336b51214
nkaroly03 <nizderkaroly03@gmail.com>
2026-04-04 19:00:43 +0200

    Corrected parse tree ascii art, added more

--- Parse/Parser.cs
+++ Parse/Parser.cs
@@ -1,40 +1,91 @@
 ﻿namespace Parse;
 
-public class Node{
-    public required Node? lhs{ get; init; }
-    public required Node? rhs{ get; init; }
+public sealed class Node{
+    public required Node? prev{ get; init; }
     public required Lex.Token token{ get; init; }
+    public required List<Node> subtrees{ get; init; }
 }
 
 public sealed class Parser{
+    List<Node> m_nodes;
+
     public Parser(List<Lex.Token> tokens){
-        
+        m_nodes = new(); 
+
+
     }
     public Parser(Lex.Lexer lexer) : this(lexer.get_tokens()){}
 
     /*
-
-                                  let
-                                   \
-                                    \
-                                     \
-                                      =
-                                     / \
+    -----------------------------------------------------------------------
+                                    let
+                                    / \
+                                   /   \
+                                  /     \
+                                 b    float
+                                        /
+                                       +
+                                      / \
+                                     /   \
+                                    /     \
+                                   /       \
+                                 3.14       *
+                                           / \
+                                          /   \
+                                         a     +
+                                              / \
+                                             /   \
+                                            a     a
+    -----------------------------------------------------------------------
+                                     print
+                                      /
+                                     a
+    -----------------------------------------------------------------------
+                                      let
+                                      /  \
+                                    c44  int
+                                         /
+                                       scan
+                                        /
+                                    "Input: "
+    -----------------------------------------------------------------------
+                                       if
+                                      /  \
+                                     /    \
+                                    /      \
+                                   /        \
+                                  >=        [0]
+                                 /  \        |
+                                /    \     return
+                               /      \     /
+                              /        \   c
+                             c         10
+    -----------------------------------------------------------------------
+                                     else
+                                      |
+                                     [0]
+                                      |
+                                    while
                                     /   \
                                    /     \
                                   /       \
                                  /         \
-                               float        +
-                                           / \
-                                          /   \
-                                         /     \
-                                        /       \
-                                      3.14       *
-                                                / \
-                                               a   +
-                                                  / \
-                                                 /   \
-                                                a     a
-
+                               and         [0]-----[1]
+                               / \          |       |
+                              /   \       print     =
+                             /     \       /       / \
+                            /       \     c       c   +
+                           /         \               / \
+                          /           \             c   1
+                         /             \        
+                        /               \               
+                       <                or               
+                      / \              /  \
+                     c  10            /    \
+                                     /      \
+                                    >=       and
+                                   / \      /   \
+                                  c   0   false true
+    -----------------------------------------------------------------------
     */
 }
